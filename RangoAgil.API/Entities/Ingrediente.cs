79cea057816c083dd8e0f07851603e4b6256fadb
igorm <igorm@BOOK-877EMP6TEK>
2024-06-06 21:03:10 -0300

    Add project files.

---
+++ RangoAgil.API/Entities/Ingrediente.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RangoAgil.API.Entities;
+
+public class Ingrediente
+{
+    [Key]
+    public int Id { get; set; } 
+
+    [Required]
+    [MaxLength(200)]
+    public required string Name { get; set; }
+
+
+    public ICollection<Rango> Rangos { get; set; } = [];
+    public Ingrediente()
+    {
+            
+    }
+
+    [SetsRequiredMembers]
+    public Ingrediente(int id, string name)
+    {
+        Id = id;
+        Name = name;
+
+    }
+}
+
